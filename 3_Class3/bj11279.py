3da71fd91620278f24b7b7b7a25e2cacad796d00
sihyeon3523 <sihyeon3523@gmail.com>
2022-02-15 15:14:16 +0900

    020215 S2

---
+++ 3_Class3/bj11279.py
@@ -0,0 +1,20 @@
+# 최대 힙 --> 최소힙 문제에서 -를 해주면 순서가 바뀜 
+# 3시 2분 ~
+import heapq as hq
+import sys
+
+n = int(input())
+
+a = []
+for i in range(n):
+    x = int(sys.stdin.readline())
+    # x가 0일 때 배열에서 가장 큰 값 출력 
+    if x == 0:
+        if len(a) == 0:
+            print(0)
+        else:
+            print(-hq.heappop(a))
+
+    # x가 0이 아니라면 값 넣어주기 
+    else:
+        hq.heappush(a, -x)
