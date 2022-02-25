b6c3cd2d4ec54bd7e22a7f9d00a91fefe105469e
sihyeon3523 <sihyeon3523@gmail.com>
2022-02-25 15:45:21 +0900

    220225 S1

--- 3_Class3/bj11403.py
+++ 3_Class3/bj11403.py
@@ -2,18 +2,26 @@
 # 경로 찾기 
 # i에서 j로 가는 경로가 있는지 없는지 구하는 프로그램
 #  
+'''
+플로이드 워셜 알고리즘
+하나의 정점에서 다ㅡㄴ 하나의 정
+다익스트라? 플로이드 워셜? 
+'''
 
-n = int(input())
-G = []
+#입력
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+    
+    
+#플로이드-워셜 알고리즘
+for k in range(N): #경로 for문이 가장 상위 단계여야 누락되지 않는다
+    for i in range(N):
+        for j in range(N): 
+            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
+                graph[i][j] = 1
 
-for i in range(n):
-    G.append(input().split())
-
-# 플로이드-워셜 알고리즘 
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1,n+1):
-            if G[i][j] == 1 or (G[i][k] == 1 and G[k][j]==1)
 
 #출력
 for row in graph:
